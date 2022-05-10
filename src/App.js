a1504fce817498eaad1c1af448981c5a788b2f37
FIGUREX <kennethggm12@gmail.com>
2022-05-09 19:52:36 -0600

    i added the original files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/original.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<section className="container-logo">
+				<img className="logo" src="./assets/Vector.svg" alt="Vector" />
+			</section>
+			<section className="container-input">
+				<textarea
+					id="search"
+					name="frase"
+					placeholder="Ingrese el texto aqui"
+					defaultValue={''}
+				/>
+			</section>
+			<section className="container-span_parrafo">
+				<p className="precaucion">
+					<span className="span">
+						<img src="./assets/informacion.svg" alt="i" />
+					</span>
+					Solo letras minúsculas y sin acentos
+				</p>
+			</section>
+			<section className="container-buttons">
+				<button id="button_encriptar">Encriptar</button>
+				<button id="button_desencriptar">Desencriptar</button>
+			</section>
+			<section className="container_result" id="container_result2">
+				<img
+					className="muñeco"
+					id="muñeco2"
+					src="./assets/Muñeco.svg"
+					alt="muñeco"
+				/>
+				<h1 className="mensaje_vacio" id="mensaje_vacio2">
+					Ningún mensaje fue encontrado
+				</h1>
+				<h2 className="mensaje_respuesta" id="mensaje_respuesta2">
+					Ingresa el texto que desees encriptar o desencriptar.
+				</h2>
+			</section>
+		</div>
+	);
 }
 
-export default App;
+export { App };
